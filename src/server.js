fed13d0c910b51dde481a18e3d2d5911ba559af5
silbioa480 <silbioa480@gmail.com>
2022-03-29 21:03:28 +0900

    add ws

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -10,4 +12,12 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log(socket);
+});
+
+server.listen(3000, handleListen);
