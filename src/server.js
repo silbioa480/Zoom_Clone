683a7e320a4a9b1bf9b6d357a3ebdebbaaba25f2
silbioa480 <silbioa480@gmail.com>
2022-03-29 21:20:42 +0900

    connect backend & frontend

--- src/server.js
+++ src/server.js
@@ -17,7 +17,12 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (socket) => {
-  console.log(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  });
+  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
